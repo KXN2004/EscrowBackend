93892851a46dd52e52a624846a0a98050d7f4f25
Vishal Mahajan <vism06@gmail.com>
2024-02-28 22:09:22 +0530

    added db

--- models/users.py
+++ models/users.py
@@ -8,5 +8,20 @@ class Users(Base):
 
     name = Column(String)
     email = Column(String, primary_key=True)
+    password = Column(String)
     phone = Column(String)
     storyline = Column(Integer)
+
+
+vishal = Users()
+
+vishal.name = "Vishal"
+vishal.email = "vism06@gmail.com"
+phone = "8999972216"
+vishal.password = phone
+vishal.phone = phone
+vishal.storyline = 1
+
+database.add(vishal)
+database.commit()
+
